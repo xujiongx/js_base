62adad537601144f720f65f1b99968bb7b06f8fb
xujiongx <2174345836@qq.com>
2020-06-20 23:18:42 +0800

    隐式类型转换

---
+++ 隐式类型转换/demo.js
@@ -0,0 +1,11 @@
+let a=[]
+let b='0'
+console.log(a.valueOf().toString()==0,Number([]),0=='0');
+console.log(Boolean([]));
+
+
+console.log(a==0);
+console.log(a==!a);
+console.log(b==0);
+console.log(a==b);
+
